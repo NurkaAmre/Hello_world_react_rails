b49bf5c757b94ee67ffb23a0433cf2d1ec9afa0d
NurkaAmre <nurkerey30@gmail.com>
2023-03-02 11:31:29 +0600

    Fix linter rubocop errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-
 module Api
   module V1
     # my comment
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # my
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
